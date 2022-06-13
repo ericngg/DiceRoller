88c5806e8eb1cea605ee82848e7191eb1ac982eb
Eric Ng <EricNg109@gmail.com>
2022-06-13 11:07:02 -0700

    Lesson 1

---
+++ app/src/main/java/project/eric/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package project.eric.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import project.eric.diceroller.databinding.ActivityMainBinding
+import java.util.*
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnRoll.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        binding.ivDice.setImageResource(drawableResource)
+
+    }
+}
